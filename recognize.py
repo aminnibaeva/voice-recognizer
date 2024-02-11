c27beedc488706820a5dbcec069e5e795955acf1
Alina <72309425+aminnibaeva@users.noreply.github.com>
2024-02-11 22:50:08 +0300

    Initial commit

---
+++ recognize.py
@@ -0,0 +1,23 @@
+from io import BytesIO
+
+import speech_recognition as sr
+from flask import Flask, request
+from pydub import AudioSegment
+
+app = Flask(__name__)
+recognizer = sr.Recognizer()
+
+
+@app.route('/recognize', methods=['POST'])
+def read_document():
+    data = request.data
+    audio = AudioSegment.from_file(BytesIO(bytearray(data)))
+    wav_data = audio.export(format="wav")
+
+    with sr.AudioFile(wav_data) as source:
+        audio_data = recognizer.record(source)
+        return recognizer.recognize_google(audio_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
